7054b4bacb5a241fffa1db53a0b16ebe06d131b3
RYTON <ryton.jiang@gmail.com>
2020-05-10 15:36:57 +0800

    完成修改定位功能及其定位的本地存储

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,41 @@
     />
 
     <div class="app-view">
-      <keep-alive>
-        <transition name="van-slide-right" mode="out-in">
+      <!-- <transition name="van-slide-right" mode="out-in">
+        <keep-alive>
           <router-view />
-        </transition>
-      </keep-alive>
+        </keep-alive>
+      </transition>
       <transition name="van-slide-right" mode="out-in">
         <router-view name="detail"></router-view>
+      </transition> -->
+
+      <transition name="van-slide-right" mode="out-in">
+        <keep-alive exclude="Detail">
+          <router-view />
+          <router-view name="detail"></router-view>
+        </keep-alive>
       </transition>
+
+      <!-- <div>
+        <transition :name="transitionName">
+          <keep-alive>
+            <router-view
+              :class="homeClass ? 'child-view ss-keep' : 'child-view1 ss-keep'"
+              v-if="$route.meta.keepAlive"
+            ></router-view>
+
+            <router-view
+              :class="homeClass ? 'child-view ns-keep' : 'child-view1 ns-keep'"
+              v-if="!$route.meta.keepAlive"
+              :key="'time' + new Date().getTime()"
+            ></router-view>
+          </keep-alive>
+        </transition>
+      </div>
+      ———————————————— 版权声明：本文为CSDN博主「漫步星空ZL」的原创文章，遵循CC
+      4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+      原文链接：https://blog.csdn.net/a13706935773/java/article/details/90082275 -->
     </div>
 
     <van-tabbar placeholder route active-color="#F39C12">
